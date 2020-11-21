09f83ba5412e2cf4ab6315ad66bbd880440a8c79
Mahdi Adnan Krayem <60859454+mahdiadnankrayem@users.noreply.github.com>
2020-11-20 23:55:08 -0500

    Cursor Fix Dragons

--- src/views/Dragons.vue
+++ src/views/Dragons.vue
@@ -16,11 +16,9 @@
       <div class="section">
         <div class="columns is-multiline is-3" id="body">
           <div class="column is-3" v-for="dragon in allDragons" :key="dragon.id">
-            <!-- <router-link :to="{name: 'Edit', params: { dragonId: dragon.id } }"> -->
-              <div class="router-click" @click="routeToEdit(dragon.id)">
+              <div class="router-click is-clickable" @click="routeToEdit(dragon.id)">
                 <Dragon :dragon="dragon" :scale="300" :isNameVisible="true"/>
               </div>
-            <!-- </router-link> -->
           </div>
         </div>
       </div>
